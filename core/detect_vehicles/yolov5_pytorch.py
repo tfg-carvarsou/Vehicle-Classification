60d535d90d6bfa4e4ca282d1ff56cca4bc17df87
carvarsou <carvarsou@alum.us.es>
2024-04-07 14:14:12 +0200

    refactor(detect): py file structure and yolov5 dependencies

--- core/detect_vehicles/yolov5_pytorch.py
+++ core/detect_vehicles/yolov5_pytorch.py
@@ -1,42 +1,73 @@
 import torch
-import yaml
 import os
+import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 
 def load_model():
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-    return model
+    torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload=True)
 
-def define_parameters():
-    with open('datasets/vehicles/yolov5_pytorch/data.yaml', 'r') as r:
-        data = yaml.safe_load(r)
-        num_classes = data['nc']
-        names = data['names']
-    return num_classes, names
+def get_model_path():
+    pwd = os.getcwd()
+    myuser = os.path.dirname(pwd)
+    return os.path.join(myuser, '.cache/torch/hub/ultralytics_yolov5_master/')
 
-def detect_objects(model, img_path, save_dir):
-    results = model(img_path)
-    results.print()
-    plt.imshow(np.squeeze(results.render()), extent=[0, 640, 480, 0])
-    plt.axis('off')
-    
-    filename = os.path.basename(img_path)
-    save_path = os.path.join(save_dir, filename)
-    plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
+def train(model_path, img_size, batch, epochs, data_path):
+    '''
+    python /home/carvarsou/.cache/torch/hub/ultralytics_yolov5_master/train.py 
+    --img 640 
+    --batch 16 
+    --epochs 5 
+    --data ./datasets/vehicles/yolov5_pytorch/data.yaml
+    '''
+    subprocess.run([
+        "python",
+        f"{model_path}train.py",
+        "--img", f"{img_size}",
+        "--batch", f"{batch}",
+        "--epochs", f"{epochs}",
+        "--data", f"{data_path}data.yaml"
+    ])
+
+def detect_vehicles(model_path, weights, img_size, conf, img):
+    '''
+    python /home/carvarsou/.cache/torch/hub/ultralytics_yolov5_master/detect.py 
+    --weights /home/carvarsou/.cache/torch/hub/ultralytics_yolov5_master/runs/train/exp3/weights/best.pt 
+    --img 640 
+    --conf 0.3 
+    --source /home/carvarsou/Vehicle-Classification/datasets/vehicles/yolov5_pytorch/test/images/adit_mp4-5_jpg.rf.bd945716e20cb3f850e2ad36df03d6e3.jpg
+    '''
+    subprocess.run([
+        "python",
+        f"{model_path}detect.py",
+        "--weights", f"{weights}",
+        "--img", f"{img_size}",
+        "--conf", f"{conf}",
+        "--source", f"{img}"
+    ])
 
 def main():
-    model = load_model()
-    base_dir = 'datasets/vehicles/yolov5_pytorch'
-    train_dir = os.path.join(base_dir, 'train')
-    img_dir = os.path.join(train_dir, 'images')
+    # 1. Download roboflow-vehicles dataset
+    # download_dataset()
 
-    filename = 'pagi_16112021_mp4-581_jpg.rf.00c56fa5c3ce82f9d92e6e5ff9367430.jpg'
-    img_path = os.path.join(img_dir, filename)
+    # 2. Load pre-trained YoloV5 model 
+    load_model()
+    model_path = get_model_path()
+    print(model_path)
 
-    save_dir = os.path.join('core', 'imgs')
-    detect_objects(model, img_path, save_dir)
+    # 3. Configure training file (data.yaml)
+    # configure_yaml()
+
+    # 4. Train model with training file
+    # train(model_path)
+
+    # 5. Evaluate new model performance
+    # evaluate()
+
+    # 6. Detect vehicles in new image
+    # detect_vehicles()
 
 if __name__ == "__main__":
     main()
+
 
