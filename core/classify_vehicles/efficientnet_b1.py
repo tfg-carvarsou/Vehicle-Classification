f81c49bf4448f914e23866a276d9eda231a5ad7c
carvarsou <carvarsou@alum.us.es>
2024-05-11 15:10:32 +0200

    feat(classify): efficientnet_b1 preprocessing

--- core/classify_vehicles/efficientnet_b1.py
+++ core/classify_vehicles/efficientnet_b1.py
@@ -1,20 +1,16 @@
 import subprocess
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-import tensorflow as tf
-tf.config.set_visible_devices([], 'GPU')
-tf.compat.v1.enable_eager_execution()
-
 import matplotlib.pyplot as plt
-from keras.applications import EfficientNetB1
-# from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import image_dataset_from_directory
-from keras import Sequential
-from keras.layers import RandomFlip, RandomRotation
-from keras.layers.experimental.preprocessing import Rescaling, Resizing
-from keras.preprocessing.image import load_img
-AUTOTUNE = tf.data.AUTOTUNE
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from StanfordDataset import StanfordDataset
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from efficientnet_pytorch import EfficientNet
+from PIL import Image
 
 # The Cars dataset contains 16,185 images of 196 classes of cars. 
 # The data is split into 8,144 training images and 8,041 testing images, 
@@ -25,43 +21,28 @@ def download_dataset(script):
     subprocess.run(["chmod", "+x", script])
     subprocess.run([script])
 
-def preprocess_dataset(train_dir):
-    # Load datasets
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        train_dir,
-        label_mode='categorical',
-        batch_size=32,
-        image_size=(224, 224),  # Adjusted image size for simplicity
-        shuffle=True
-    )
+def preprocess_dataset(csv_file, train_dir):
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-    # Resize and rescale datasets
-    resize_and_rescale = Sequential([Resizing(224, 224), Rescaling(1./255)])
-    train_ds = train_ds.map(lambda x, y: (resize_and_rescale(x), y), num_parallel_calls=AUTOTUNE)
-    return train_ds.prefetch(buffer_size=AUTOTUNE)
+    train_ds = StanfordDataset(csv_file=csv_file, root_dir=train_dir, transform=transform)
+    train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
+    return train_loader
 
-def print_image(image_path):
-    image = load_img(image_path)
-    plt.imshow(image)
-    plt.show()
-
-def plot_images(dataset, num_images=5):
-    plt.figure(figsize=(10, 10))
-    for images, labels in dataset.take(1):  # Take one batch
-        for i in range(num_images):
-            ax = plt.subplot(5, 5, i + 1)
-            image = images[i].numpy().astype("uint8")
-            label = labels[i].numpy()
-            if image.max() == 0:
-                print(f"Image {i+1}: All pixels are black. Label: {label}")
-            else:
-                plt.imshow(image)
-                plt.title(label)
-                plt.axis("off")
-    plt.show()
+def print_dataset_samples(dataset_loader, num_samples=1):
+    for i, (images, _, class_labels, metadata) in enumerate(dataset_loader):
+        if i > num_samples-1:
+            break
+        for j in range(num_samples):
+            plt.imshow(images[j].permute(1, 2, 0))
+            plt.title(f"Make: {metadata['make'][j]}, Model: {metadata['model'][j]}, Year: {metadata['year'][j]}, Label: {class_labels[j]}")
+            plt.show()
 
 def debug_mode():
-    download = True
+    download = False
     preprocess = True
     load = False
     train = False
@@ -72,19 +53,18 @@ def debug_mode():
 def main():
     download, preprocess, load, train, evaluate, classify = debug_mode()
     train_dir = './datasets/stanford/car_data/car_data/train'
+    train_csv = './datasets/stanford/anno_train.csv'
     test_dir = './datasets/stanford/car_data/car_data/test'
+    test_csv = './datasets/stanford/anno_test.csv'
 
-    # 1. Download stanford dataset
     if download:
         print("\n📥 Downloading dataset...")
         download_dataset('./scripts/stanford_dataset.sh')
     
-    # 2. Preprocess data
     if preprocess:
         print("\n⏳ Preprocessing data...")
-        # print_image('./datasets/stanford/car_data/car_data/train/Acura Integra Type R 2001/00198.jpg')
-        train_ds = preprocess_dataset(train_dir)
-        plot_images(train_ds, 2)
+        train_loader = preprocess_dataset(train_csv, train_dir)
+        print_dataset_samples(train_loader)
         print("Data preprocessed successfully.")
 
 if __name__ == "__main__":
