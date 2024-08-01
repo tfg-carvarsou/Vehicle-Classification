40d3b9ab58766118904296e8c102cd1ff524b576
Carlos Varela Soult <carvarsou@alum.us.es>
2024-08-01 13:14:54 +0200

    chore(backend): load custom yolov5s model

--- webapp/vc_backend/vc_backend_app/views.py
+++ webapp/vc_backend/vc_backend_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import sys, os
+sys.path.append(os.getcwd())
+from django.conf import settings
+from core.detect_vehicles.yolov5.model import load_trained_yolov5s_model
+from core.detect_vehicles.yolov5.yv5_detect import detect_vehicles
+from PIL import Image
 
-# Create your views here.
+detect_yolov5s_model = load_trained_yolov5s_model()
+
+test_img = Image.open('./images/detect_vehicles/yolov5/camiones.jpg')
+res = detect_yolov5s_model(test_img)
+res.show()
