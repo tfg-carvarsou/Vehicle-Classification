a75482387dbd227ac020cd2e589f81aa1327db16
Carlos Varela Soult <carvarsou@alum.us.es>
2024-08-02 12:54:29 +0200

    feat(backend): file upload

--- webapp/vc_backend/vc_backend_app/views.py
+++ webapp/vc_backend/vc_backend_app/views.py
@@ -1,55 +1,38 @@
-import sys, os, io, base64
-sys.path.append(os.getcwd())
-from django.shortcuts import render
+import sys, os, io
 from .forms import ImageUploadForm
 from .models import VDImage
 from .serializers import VDImageSerializer
 from torchvision import transforms
-from core.detect_vehicles.yolov5.model import load_trained_yolov5s_model
 from PIL import Image
-from rest_framework import viewsets
-
-detect_yolov5s_model = None 
-# detect_yolov5s_model = load_trained_yolov5s_model()
-
-# img = Image.open('./images/classify_vehicles/efficientnet_b1/Aston Martin V8 Vantage Convertible 2012.jpg')
-# img = transforms.Resize((720, 1280))(img)
-# img = detect_yolov5s_model(img)
-# img.show()
-
-def transform_image(image):
-    tf = transforms.Compose([
-        transforms.Resize((720, 1280))
-    ])
-    return tf(image)
-
-def get_prediction(image_bytes):
-    image = Image.open(io.BytesIO(image_bytes))
-    image = transform_image(image)
-    return detect_yolov5s_model(image)
+from django.http import JsonResponse
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 class VDImageViewSet(viewsets.ModelViewSet):
     queryset = VDImage.objects.all()
     serializer_class = VDImageSerializer
 
-def detector(request):
-    image_uri = None
-    if request.method == 'POST':
+    @extend_schema(request=VDImageSerializer, responses=VDImageSerializer)
+    def create(self, request, *args, **kwargs):
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.cleaned_data['image']
-            image_bytes = image.file.read()
-            encoded_img = base64.b64encode(image_bytes).decode('ascii')
-            image_uri = f'data:image/jpeg;base64,{encoded_img}'
-            try:
-                res = get_prediction(image_bytes)
-            except RuntimeError as e:
-                print(e)
+            transformed_image = self.transform_image(image)
+            vd_image = VDImage(image=image)
+            vd_image.image.save(f"{image.name.split('.')[0]}_transformed.jpg", transformed_image, save=True)
+
+            serializer = VDImageSerializer(vd_image)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            form = ImageUploadForm()
-                
-    context = {
-        'form': form,
-        'image_uri': image_uri,
-        'image': res
-    }
-    return render(request, 'vc_backend_app/detector.html', context)
+            return JsonResponse(form.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def transform_image(self, image_file):
+        image = Image.open(image_file)
+        transform = transforms.Compose([
+            transforms.Resize((720, 1280))
+        ])
+        transformed_image = transform(image)
+        image_io = io.BytesIO()
+        transformed_image.save(image_io, format='JPEG')
+        image_io.seek(0)
+        return image_io
