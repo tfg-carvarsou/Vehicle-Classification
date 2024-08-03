f6ed9507591ab156fc9896a12ce57445d8f19971
Carlos Varela Soult <carvarsou@alum.us.es>
2024-08-03 12:59:21 +0200

    fix(detector): fix minimal form error

--- webapp/vc_backend/vc_backend_app/forms.py
+++ webapp/vc_backend/vc_backend_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class VDImageUploadForm(forms.Form):
-    model = forms.ChoiceField(choices=[('YOLOv5s', 'R-CNN')])
+    model = forms.ChoiceField(choices=[('YOLOv5s', 'YOLOV5S'), ('R-CNN', 'RCNN')])
     image = forms.ImageField()
